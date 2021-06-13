19eac193e18885b313844f7787ad8ee0a2984d90
Prokorsho Munshi <61050881+backchodpm@users.noreply.github.com>
2021-06-13 12:11:00 +0530

    Update forest_fire.py

--- forest_fire.py
+++ forest_fire.py
@@ -26,7 +26,6 @@ X = np.array(X)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=20)
 log_reg = LogisticRegression()
 log_reg.fit(X_train, Y_train)
-inputt=[int(x) for x in "0 0 0 0".split(' ')]
-final=[np.array(inputt)]
+]
 pickle.dump(log_reg,open('model.pkl','wb'))
 model=pickle.load(open('model.pkl','rb'))
